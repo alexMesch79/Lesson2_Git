17396f4552a4ed80cf3561a334cce473893f9cec
AlexeyM <alex_mesch@mail.ru>
2022-05-25 20:07:37 +0300

    first_commit

---
+++ src/test/java/guru/qa/Test_lesson2.java
@@ -0,0 +1,12 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Test_lesson2 {
+    @Test
+    void Git_FirstTest() {
+        Assertions.assertTrue(100 > 1);
+
+    }
+}
