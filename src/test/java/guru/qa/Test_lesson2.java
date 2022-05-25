83d516d259b46f38834742425e18ca578eb7b840
AlexeyM <alex_mesch@mail.ru>
2022-05-25 21:40:26 +0300

    fifth_commit_IDE, changed Test1 and Test2

--- src/test/java/guru/qa/Test_lesson2.java
+++ src/test/java/guru/qa/Test_lesson2.java
@@ -6,12 +6,12 @@ import org.junit.jupiter.api.Test;
 public class Test_lesson2 {
     @Test
     void Git_FirstTest() {
-        Assertions.assertTrue(150 > 1);
+        Assertions.assertTrue(150 > 149);
 
     }
     @Test
     void Git_SecondTest() {
-        Assertions.assertTrue(150 < 200);
+        Assertions.assertTrue(150 < 151);
 
     }
     @Test
